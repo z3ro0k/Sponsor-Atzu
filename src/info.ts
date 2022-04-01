b5e013cc7a85808e8480264f2f078983d42018a6
z3ro0k <toxictoxd@gmail.com>
2022-04-01 14:47:49 -0600

    Files daa

---
+++ src/info.ts
@@ -0,0 +1,19 @@
+export const information = {
+    // What's your GitHub username?
+    username: "z3ro0k",
+
+    // Highlight some of your best works by providing your repository NAMES.
+    featuredRepos: ["namae", "NextJs-Lanyard ", "guide-vercel-express", "docs-es"],
+
+    // Describe yourself, and why you should be sponsored.
+    description: `Hi, my name is Alexis, but known as Atzu or z3ro0k over the internet and I am a 20 year old "fullstack" developer that spends time coding and building websites. I've been coding for around 5 years now and still on the road to learn more.
+
+I know the following languages: JavaScript, TypeScript, C#, Kotlin, Java, and Python
+    `,
+
+    // Where should they be directed to pay you?
+    payment_url: "https://www.paypal.com/paypalme/Atzu",
+
+    // Only set to true if your URL supports an amount at the end, e.g. https://paypal.com/paypalme/username/20, otherwise set it to false.
+    amount_slug: true,
+};
